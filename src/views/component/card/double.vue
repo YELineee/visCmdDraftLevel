dda8dc1ede1e75499afcfbaba6bddaf52412ce35
YEL!ne <1638317920@qq.com>
2025-01-13 15:32:17 +0800

    feat: update double component to display max and min

--- src/views/component/card/double.vue
+++ src/views/component/card/double.vue
@@ -2,25 +2,45 @@
    <div class="mb-4 rounded-2xl break-inside-avoid font-Montserrat grid grid-cols-2 gap-4 text-black">
       <div class=" w-full h-[220px] bg-slate-200 rounded-2xl p-4">
          <div class="text-6xl">
-            10%
+            {{ maxHourCount.count }}
+         </div>
+         <div class="text-xl">
+            {{ maxHourCount.timePeriod }}
          </div>
       </div>
       <div class=" w-full h-[220px] bg-slate-200 rounded-2xl p-4">
          <div class="text-6xl">
-            10%
+            {{ minHourCount.count }}
+         </div>
+         <div class="text-xl">
+            {{ minHourCount.timePeriod }}
          </div>
       </div>
    </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import { useHistoryDataStore } from '../../../stores/store';
 import { storeToRefs } from 'pinia';
 
 const historyDataStore = useHistoryDataStore();
 const { analyzeData } = storeToRefs(historyDataStore);
 
-// console.log(analyzeData.value.yearlyData);
-// console.log(typeof(analyzeData.value.yearlyData));
+const maxHourCount = computed(() => {
+    const currentYear = 2024;
+    const hourlyData = analyzeData.value.hourlyData[currentYear] || {};
+    const maxCount = Math.max(...Object.values(hourlyData));
+    const timePeriod = Object.keys(hourlyData).find(key => hourlyData[key] === maxCount);
+    return { count: maxCount, timePeriod };
+});
+
+const minHourCount = computed(() => {
+    const currentYear = 2024;
+    const hourlyData = analyzeData.value.hourlyData[currentYear] || {};
+    const minCount = Math.min(...Object.values(hourlyData));
+    const timePeriod = Object.keys(hourlyData).find(key => hourlyData[key] === minCount);
+    return { count: minCount, timePeriod };
+});
 
 </script>
